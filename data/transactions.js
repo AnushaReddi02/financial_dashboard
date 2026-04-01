d92d356cab1b94f7e6dba6d2191ea867a809167b
AnushaReddi02 <anushareddi115@gmail.com>
2026-04-01 18:45:24 +0530

    Added Mock Data

---
+++ data/transactions.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { date: "2026-04-01", amount: 5000, category: "Salary", type: "income" },
+  { date: "2026-04-02", amount: 200, category: "Food", type: "expense" },
+  { date: "2026-04-03", amount: 1000, category: "Shopping", type: "expense" },
+  { date: "2026-04-04", amount: 300, category: "Transport", type: "expense" },
+];
